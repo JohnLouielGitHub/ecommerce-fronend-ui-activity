b7f73eb084a40b8f1e67aba03c0f588ba4418ff4
codeLou <tamargo.louiel@gmail.com>
2021-06-27 23:13:37 +0800

    2nd commit

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
+import Image from 'next/image';
 
 function CartItem({ item, removeFromCart }) {
   const [productCount, setProductCount] = useState(1);
@@ -6,11 +7,10 @@ function CartItem({ item, removeFromCart }) {
   return (
     <div className="flex justify-between mt-6">
       <div className="flex">
-        <img
+        <Image           
           className="h-20 w-20 object-cover rounded"
           src={item.img_url}
-          alt=""
-        />
+          alt=""/>
         <div className="mx-3">
           <h3 className="text-sm text-gray-600">{item.name}</h3>
           <div className="flex items-center mt-2">
@@ -61,15 +61,15 @@ function CartItem({ item, removeFromCart }) {
         <button onClick={() => removeFromCart(item.id)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
             />
           </svg>
